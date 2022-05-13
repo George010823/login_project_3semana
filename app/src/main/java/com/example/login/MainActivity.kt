9adbdbe4d6cb12a9e2b1a024811770c055f4f6f5
Jorge Ivan Gomez <jorgomezre@unal.edu.co>
2022-05-13 14:04:16 -0500

    Desarrollo Restricciones Version1

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        validarCampos()
+    }
+
+    private fun validarCampos() {
+        var editText_Nombre: EditText = findViewById(R.id.editText_Nombre)
+        var editText_Passw: EditText = findViewById(R.id.editText_Passw)
+        val buttonLogin: Button = findViewById(R.id.btn_login)
+        buttonLogin.setOnClickListener(){
+            if (editText_Nombre.getText().isEmpty() and  editText_Passw.getText().isEmpty()){
+                Toast.makeText(this, "No se puede realizar la acción porque los campos Nombre y Contarseña son obligatorios", Toast.LENGTH_LONG).show()
+            }else if (editText_Nombre.getText().isEmpty()){
+                Toast.makeText(this, "No se puede realizar la acción porque el campo Nombre es obligatorio", Toast.LENGTH_LONG).show()
+            }else if (editText_Passw.getText().isEmpty()){
+                Toast.makeText(this, "No se puede realizar la acción porque el campo Contraseña es obligatorio", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
